5e3d054dbbe974c2cb959a10210efd61c8185078
rpalomino <rpalomino@homeaway.com>
2020-05-08 18:14:16 +0200

    New Bars color based on temp

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -10,7 +10,38 @@ import "moment/locale/ru";
 import { LanguageContext } from "../../Context";
 import "./Forecast.css";
 
-const renderForecastDay = list => {
+const bgColors = {
+  verywarm: "rgba(255, 130, 20, 1)",
+  warm: "rgba(240, 216, 0, 1)",
+  normal: "rgba(62, 179, 62, 1)",
+  cold: "rgba(0, 255, 244, 1)",
+  verycold: "rgba(0, 164, 255, 1)"
+};
+
+const getTempColor = (temp, metrics) => {
+  const MAX_VERYWARN = metrics === "metric" ? 40 : 104;
+  const MAX_WARN = metrics === "metric" ? 30 : 86;
+  const MAX_NORMAL = metrics === "metric" ? 20 : 68;
+  const MAX_COLD = metrics === "metric" ? 10 : 50;
+  const MAX_VERYCOLD = metrics === "metric" ? 0 : 32;
+  if (temp >= MAX_VERYWARN) {
+    return bgColors.warm;
+  }
+  if (MAX_NORMAL <= temp && temp < MAX_WARN) {
+    return bgColors.warm;
+  }
+  if (MAX_COLD <= temp && temp < MAX_NORMAL) {
+    return bgColors.normal;
+  }
+  if (MAX_VERYCOLD <= temp && temp < MAX_COLD) {
+    return bgColors.cold;
+  }
+  if (temp < MAX_VERYCOLD) {
+    return bgColors.verycold;
+  }
+};
+
+const renderForecastDay = (list, settings) => {
   const max = list
     .map(day => day.max)
     .reduce((a, b) => {
@@ -24,7 +55,11 @@ const renderForecastDay = list => {
         <h6>{moment(day.date).format("ddd, Do")}</h6>
         <div className="dayForecast">
           <span className="min">{day.min}</span>
-          <span className="max" style={{ width: `${percent}%` }}>
+          <span className="max" 
+            style={{
+              width: `${percent}%`,
+              backgroundColor: getTempColor(day.max, settings) 
+          }}>
             {day.max}
           </span>
         </div>
@@ -34,12 +69,12 @@ const renderForecastDay = list => {
 };
 
 const Forecast = props => {
-  const { list } = props;
+  const { list, settings } = props;
   const { lang } = useContext(LanguageContext);
   moment.locale(lang);
   return (
     <div className="Forecast">
-      <div className="graphic">{renderForecastDay(list)}</div>
+      <div className="graphic">{renderForecastDay(list, settings)}</div>
     </div>
   );
 };
