7181d218f250dc17a063b1a85a28457e36abf2c3
Ruben <ruben_munoz_palomino@hotmail.com>
2024-05-01 16:25:39 +0200

    update react to 18.3.1 and update npm (#41)
    Co-authored-by: Ruben Munoz Palomino <rpalomino@expediagroup.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
