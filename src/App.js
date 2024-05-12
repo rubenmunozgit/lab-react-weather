785b8dea01dca691a9f72910ee5c6ae1817f2bad
Ruben Munoz Palomino <rpalomino@expediagroup.com>
2024-05-12 16:18:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { LanguageContext } from './contexts/LanguageContext';
-import Loading from './components/Loading/Loading';
-import Current from './components/Current/Current';
-import Forecast from './components/Forecast/Forecast';
-import Nav from './components/Nav/Nav';
+import React, { useContext } from 'react';
 import './styles.css';
 
-import useFetchWeather from './hooks/useFetchWeather';
-import useBrowserLocation from './hooks/useBrowserLocation';
 import AccessingLocation from './components/Loading/AccessingLocation';
 import AlertLocation from './components/Loading/AlertLocation';
 import WeatherApp from './Pages/WeatherApp';
