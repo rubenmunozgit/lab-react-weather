48b2e48421d7753f84fa03ccb151df265effabaf
Ruben <ruben_munoz_palomino@hotmail.com>
2024-11-08 20:36:15 +0100

    Improve MaxMinByDate (#70)
    Co-authored-by: Ruben Munoz Palomino <rpalomino@expediagroup.com>

--- src/mutations/maxMinByDate.js
+++ src/mutations/maxMinByDate.js
@@ -1,37 +1,23 @@
 import { dateFormat } from './dateFomat';
 
 const getMaxMinByDate = (list) => {
-  const aggregateByDate = list
-    .map((wPoint) => ({
-      date: dateFormat(wPoint.dt_txt.replace(/-/g, '/')),
-      temp: wPoint.main.temp.toFixed(1),
-    }))
-    .reduce((groups, item) => {
-      const group = groups[item.date] || { date: groups[item.date], temps: [] };
-      group.date = item.date;
-      group.temps.push(item.temp);
-      groups[item.date] = group;
-      return groups;
-    }, {});
+  const aggregateByDate = list.reduce((groups, wPoint) => {
+    const date = dateFormat(wPoint.dt_txt.replace(/-/g, '/'));
+    const temp = parseFloat(wPoint.main.temp.toFixed(1));
+    if (!groups[date]) {
+      groups[date] = { date, temps: [] };
+    }
+    groups[date].temps.push(temp);
+    return groups;
+  }, {});
 
-  const groupByDateArray = Object.keys(aggregateByDate)
-    .map((date) => ({
-      date,
-      temps: aggregateByDate[date].temps,
-    }))
+  return Object.values(aggregateByDate)
+    .map(({ date, temps }) => {
+      const min = Math.min(...temps);
+      const max = Math.max(...temps);
+      return { date, min, max };
+    })
     .slice(1);
-
-  const tempMaxMinByDate = groupByDateArray.map((curr) => {
-    const maxMinByDate = curr.temps.reduce((agg, currEle) => {
-      agg.date = curr.date;
-      agg.min = agg.min === undefined || currEle < agg.min ? currEle : agg.min;
-      agg.max = agg.max === undefined || currEle > agg.max ? currEle : agg.max;
-      //agg[datesTemps[i].date] = minMax
-      return agg;
-    }, {});
-    //console.log(x)
-    return maxMinByDate;
-  });
-  return tempMaxMinByDate;
 };
+
 export default getMaxMinByDate;
