bccab1baa3ce69a98578155903505b71b9a942d8
Ruben Munoz Palomino <ruben_munoz_palomino@hotmail.com>
2025-01-24 14:36:26 +0100

    Adds a tooltip with weather info

---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,33 @@
+import { SvgInfo } from '../svgs/SvgInfo';
+import { useEffect, useRef } from 'react';
+import { useTooltipShow } from '../../hooks/useTooltipShow';
+
+export const Tooltip = ({ children }) => {
+  const tooltipRef = useRef(null);
+  const { isVisible, onMouseEnter, onMouseLeave } = useTooltipShow();
+
+  useEffect(() => {
+    tooltipRef.current.addEventListener('mouseenter', onMouseEnter);
+    tooltipRef.current.addEventListener('mouseleave', onMouseLeave);
+    return () => {
+      tooltipRef.current.removeEventListener('mouseenter', onMouseEnter);
+      tooltipRef.current.removeEventListener('mouseleave', onMouseLeave);
+    };
+  }, [onMouseEnter, onMouseLeave]);
+
+  return (
+    <div
+      className="d-inline-flex align-baseline position-relative"
+      ref={tooltipRef}
+    >
+      <div className="mx-2">
+        <SvgInfo />
+      </div>
+      {isVisible && (
+        <div className="position-absolute top-0 end-100 bg-light border border-secondary rounded p-2 z-1">
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ src/components/Tooltip/TooltipContent.js
@@ -0,0 +1,40 @@
+import { SvgSunHours } from '../svgs/SvgSunHours';
+import React, { useContext } from 'react';
+import { LanguageContext } from '../../contexts/LanguageContext';
+import { SettingsContext } from '../../contexts/SettingsContext';
+import { units } from '../../utils/units';
+import { InfoItem } from './InfoItem';
+
+export const TooltipContent = (props) => {
+  const { translatedText } = useContext(LanguageContext);
+  const { unit } = useContext(SettingsContext);
+  const { humidity, wiSpeed, sunHours } = props;
+  const windMetrics = units[unit].speed;
+
+  const content = [
+    [
+      { label: translatedText.humidity, value: `${humidity}%` },
+      { label: translatedText.wind, value: `${wiSpeed} ${windMetrics}` },
+    ],
+    [
+      { label: translatedText.sunrise, value: sunHours.sunrise },
+      { label: translatedText.sunset, value: sunHours.sunset },
+    ],
+  ];
+
+  return (
+    <div className="d-flex flex-column align-items-center">
+      {content.map((row, rowIdx) => (
+        <div
+          key={rowIdx}
+          className="d-flex justify-content-center align-items-center"
+        >
+          {row.map((column, colIdx) => (
+            <InfoItem key={colIdx} {...column} />
+          ))}
+        </div>
+      ))}
+      <SvgSunHours {...sunHours} />
+    </div>
+  );
+};
