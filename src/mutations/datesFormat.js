95261e1b8a9f1a96e5a4d00fd3eebca1aabe3474
Ruben <ruben_munoz_palomino@hotmail.com>
2026-07-16 18:04:54 +0000

    add date improvement

--- src/mutations/datesFormat.js
+++ src/mutations/datesFormat.js
@@ -1,22 +1,49 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import duration from 'dayjs/plugin/duration';
+
 dayjs.extend(relativeTime);
+dayjs.extend(duration);
 
-const dateFormat = (date) => {
-  return dayjs(date).format('YYYY-MM-DD');
+const DATE_FORMAT = 'YYYY-MM-DD';
+const TIME_FORMAT = 'HH:mm:ss';
+
+const dateFormat = (value) => {
+  const date = dayjs(value);
+  return date.isValid() ? date.format(DATE_FORMAT) : '';
 };
 
-const timeFormat = (dt) => {
-  return dayjs.unix(dt).format('HH:mm:ss');
+const timeFormat = (unixSeconds) => {
+  const date = dayjs.unix(unixSeconds);
+  return date.isValid() ? date.format(TIME_FORMAT) : '';
 };
 
-const sunHoursCalculations = (sunrise, sunset) => {
-  const sunriseTime = dayjs.unix(sunrise);
-  const sunsetTime = dayjs.unix(sunset);
-  const sunHoursDiffInHours = sunsetTime.diff(sunriseTime, 'hour', true);
-  const differenceInMilliseconds = sunsetTime.diff(sunriseTime);
+const sunHoursCalculations = (sunriseUnixSeconds, sunsetUnixSeconds) => {
+  const sunrise = dayjs.unix(sunriseUnixSeconds);
+  const sunset = dayjs.unix(sunsetUnixSeconds);
 
-  const sunHoursStr = dayjs(differenceInMilliseconds).format('HH:mm:ss');
+  if (!sunrise.isValid() || !sunset.isValid() || sunset.isBefore(sunrise)) {
+    return {
+      sunHoursStr: '',
+      sunHoursPct: '0.00',
+      sunHoursDiffInHours: '0.00',
+    };
+  }
+
+  const diffMs = sunset.diff(sunrise);
+  const diffDuration = dayjs.duration(diffMs);
+
+  const hours = Math.floor(diffDuration.asHours());
+  const minutes = diffDuration.minutes();
+  const seconds = diffDuration.seconds();
+
+  const sunHoursStr = [
+    String(hours).padStart(2, '0'),
+    String(minutes).padStart(2, '0'),
+    String(seconds).padStart(2, '0'),
+  ].join(':');
+
+  const sunHoursDiffInHours = diffDuration.asHours();
   const sunHoursPct = ((sunHoursDiffInHours / 24) * 100).toFixed(2);
 
   return {
@@ -26,8 +53,9 @@ const sunHoursCalculations = (sunrise, sunset) => {
   };
 };
 
-const updatedFromNow = (dt) => {
-  return dayjs().to(dayjs.unix(dt));
+const updatedFromNow = (unixSeconds) => {
+  const date = dayjs.unix(unixSeconds);
+  return date.isValid() ? dayjs().to(date) : '';
 };
 
 export { dateFormat, timeFormat, sunHoursCalculations, updatedFromNow };
